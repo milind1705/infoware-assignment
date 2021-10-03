75845ccae33193d20dee7b809dc9bff457cdc8d7
unknown <sois.milindsir@gmail.com>
2021-10-03 14:28:59 +0530

    order

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,29 +1,29 @@
-const User = require('../models/userModel');
-const Product = require('../models/productModel');
-const Order = require('../models/orderModel');
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const Order = require("../models/orderModel");
 
 module.exports.createOrder = (req, res) => {
-     const productName = req.query.name;
-      Product.find({name : productName}).then((data) =>{
-         const product = data;
-         const productPrice = { price : product.price}
+  const productId = req.query.id;
+  Product.findOne({ _id: productId }).then((data) => {
+    const product = data;
+    let productName = product.name;
+    let productPrice = product.price;
+    let quantity = req.body.quantity;
+    let totalPrice = productPrice * quantity;
 
-        return res.send(productPrice)
-         
-        });
-     //const product = productID.name
-    // const quantity = req.body.quantity;
-    // const amount = quantity * product.price;
-
-    // const newOrder = new Order({user: user.name, product : productName, quantity: quantity, toatlPrice:amount});
-
-    // newOrder
-    // .save()
-    // .then((data) => {
-    //     return res.status(200).json(data)
-    // })
-    // .catch((err) => {
-    //     return res.status(400).json(err)
-    // })
-
-}
+    let neworder = new Order({
+      productName,
+      productPrice,
+      quantity,
+      totalPrice,
+    });
+    neworder
+      .save()
+      .then((data) => {
+        return res.send(data);
+      })
+      .catch((err) => {
+        return res.send(err);
+      });
+  });
+};
